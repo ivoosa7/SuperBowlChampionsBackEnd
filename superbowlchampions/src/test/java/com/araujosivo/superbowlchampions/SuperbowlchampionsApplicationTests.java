cb34e827e0b60813766cad65f9f4aedb055d879e
Ivo Araujo <ivo.souza-23@outlook.com>
2025-02-18 12:28:19 -0300

    Iniciando Projeto e pasta entities criada

---
+++ superbowlchampions/src/test/java/com/araujosivo/superbowlchampions/SuperbowlchampionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.araujosivo.superbowlchampions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperbowlchampionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
