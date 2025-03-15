2131ad9617b38d34d648ed7efbbd52e48c6095f4
Ivo Araujo <ivo.souza-23@outlook.com>
2025-03-15 14:01:21 -0300

    Controller iniciado

---
+++ superbowlchampions/src/main/java/com/araujosivo/superbowlchampions/controllers/SbControler.java
@@ -0,0 +1,13 @@
+package com.araujosivo.superbowlchampions.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("superbowlchampions")
+public class SbControler {
+    
+
+}
