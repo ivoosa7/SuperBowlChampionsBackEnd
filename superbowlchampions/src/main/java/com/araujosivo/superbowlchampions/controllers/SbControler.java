421b2d89879edf46fc2314a8e9938cc1c3a37b27
Ivo Araujo <ivo.souza-23@outlook.com>
2025-03-17 13:01:11 -0300

    Data.sql criado e acesso ao h2 database para testes

--- superbowlchampions/src/main/java/com/araujosivo/superbowlchampions/controllers/SbControler.java
+++ superbowlchampions/src/main/java/com/araujosivo/superbowlchampions/controllers/SbControler.java
@@ -1,13 +1,20 @@
 package com.araujosivo.superbowlchampions.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.araujosivo.superbowlchampions.service.SBService;
 
 @CrossOrigin
 @RestController
 @RequestMapping("superbowlchampions")
 public class SbControler {
+    
+    @Autowired
+    public SBService service;
+
     
 
 }
---
+++ superbowlchampions/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO SBCHAMPIONS(CHAMPIONS, SCORE, OPPONENT, MVP, SEASON, LOCAL) VALUES ('Green Bay Packers', '35 x 10', 'Kansas City Chiefs', 'QB - Bart Starr', '1966', 'Los Angeles Memorial Coliseum');
+
