d2bd4cd057be74ae28c5dc806656a7f8218ecf48
Ivo Araujo <ivo.souza-23@outlook.com>
2025-04-02 09:42:34 -0300

    Controllers feitos e funcionando

--- superbowlchampions/src/main/java/com/araujosivo/superbowlchampions/controllers/SbControler.java
+++ superbowlchampions/src/main/java/com/araujosivo/superbowlchampions/controllers/SbControler.java
@@ -1,11 +1,25 @@
 package com.araujosivo.superbowlchampions.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.araujosivo.superbowlchampions.dtos.SBRequest;
+import com.araujosivo.superbowlchampions.dtos.SBResponse;
 import com.araujosivo.superbowlchampions.service.SBService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @CrossOrigin
 @RestController
@@ -15,6 +29,39 @@ public class SbControler {
     @Autowired
     public SBService service;
 
+    //1 EndPoint
+    @GetMapping
+    public ResponseEntity<List<SBResponse>> getSbChampions(){
+        return ResponseEntity.ok(service.getAllSuperbowls());
+    }
+
+    //2 EndPoint
+    @GetMapping("{id}")
+    public ResponseEntity<SBResponse> getSbChampionsById(@PathVariable long id){
+        return ResponseEntity.ok(service.getSuperBowlById(id));
+    }
+
+    //3 EndPoint
+    @PostMapping()
+    public ResponseEntity<SBResponse> saveNewSbChampions(@Validated @RequestBody SBRequest sb){
+        SBResponse newChampions = service.saveNewSbChampions(sb);
+        return ResponseEntity.created(null).body(newChampions);
+    }
+
+    //4 EndPoint
+    @PutMapping("{id}")
+    public ResponseEntity<Void> updateSbChampions(@PathVariable long id, @Validated @RequestBody SBRequest sb){
+        service.updateSbChampions(sb, id);
+        return ResponseEntity.ok().build();
+    }
+
+    //5 EndPoint
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteSbChampions(@PathVariable long id){
+        service.deleteSbChampions(id);
+        return ResponseEntity.noContent().build();
+    }
+    
     
 
 }
--- superbowlchampions/src/main/resources/data.sql
+++ superbowlchampions/src/main/resources/data.sql
@@ -1,2 +1,16 @@
-INSERT INTO SBCHAMPIONS(CHAMPIONS, SCORE, OPPONENT, MVP, SEASON, LOCAL) VALUES ('Green Bay Packers', '35 x 10', 'Kansas City Chiefs', 'QB - Bart Starr', '1966', 'Los Angeles Memorial Coliseum');
+INSERT INTO SBCHAMPIONS(CHAMPIONS, SCORE, OPPONENT, MVP, SEASON, LOCAL) 
+VALUES (
+    'Green Bay Packers', 
+    '35 x 10', 
+    'Kansas City Chiefs', 
+    'QB - Bart Starr', 
+    '1966', 
+    'Los Angeles Memorial Coliseum'),
+    ('Green Bay Packers',
+    '33 x 14',
+    'Las Vegas Raiders',
+    'QB - Bart Starr',
+    '1967',
+    'Miami Orange Bowl'
+    );
 
